54ae034a86d05fa6b01a9ad203f59b0757e64b98
turneps403 <turneps403@mail.ru>
2018-11-26 20:14:05 +0300

    there are first worked game and study mode

--- iverb.py
+++ iverb.py
@@ -1,53 +1,35 @@
 import sys
+import copy
+from random import uniform
 
-from iVerb.Tables import VictorinaTable, SimpleTable
+from iVerb.Tables import IrregularVerbs, VictorinaTable, SimpleTable
+from iVerb.Term import GetLine, HorizontalOptions
 
+def weighted_choice(choices):
+	total = sum(w for c, w in choices.items())
+	r = uniform(0, total)
+	upto = 0
+	for c, w in choices.items():
+		if upto + w >= r:
+			return c
+		upto += w
 
-
-print("hello")
-
-sys.exit()
-
-victorina = Victorina()
-score = 0
-for verb in iter(IrregularVerbs()):
-	riddle_verb = copy.deepcopy(verb)
-	# ["infinitive", "past_simple", "past_participle"]
-	expected_answer = riddle_verb["past_simple"]["verb"]
-	lenght = max([len(riddle_verb["past_simple"]["verb"]), len(riddle_verb["past_simple"]["ipa"])])
-	riddle_placeholder = " "*int((lenght - 1)/2) + '?' + " "*int((lenght - 1)/2)
-	riddle_verb["past_simple"] = {"verb": riddle_placeholder, "ipa": riddle_placeholder}
-	score = victorina.draw(verb, riddle_verb, expected_answer, score)
-
-#stable = SimpleTable()
-#for verb in iter(IrregularVerbs()):
-#	stable.draw(verb)
-#	GetLine.await_for_enter({" ": {"break": 1}, "\x1b[C": {"break": 1}})
-
-sys.exit()
-data = [
-	['Row one column one', 'Row one column two', 'eeee'],
-	['Row two column one', 'Row two column two', 'fooop'],
-]
-table = VerbAsciiTable(data)
-table.replace_draw()
-
-print("Score: 0")
-print("What: ", end='', flush=True)
-answer = GetLine.await_for_enter()
-answer = answer.strip().lower()
-if answer == "foobar":
-	table.replace_draw(1)
-	print("Score: 10")
-	print("What: FOOBAR", end='', flush=True)
-	GetLine.await_for_enter({" ": {"break": 1}})
+choice = HorizontalOptions(["study", "game"]).choice()
+if choice == 0:
+	stable = SimpleTable()
+	for verb in iter(IrregularVerbs()):
+		stable.draw(verb)
+		GetLine.await_for_enter({" ": {"break": 1}, "\x1b[C": {"break": 1}})
 else:
-	table.replace_draw(1)
-	print("Score: -10")
-	
-	print("What: "+answer[0:-1]+" X", end='', flush=True)
-	GetLine.await_for_enter({" ": {"break": 1}})
+	victorina = VictorinaTable()
+	score = 0
+	for verb in iter(IrregularVerbs()):
+		riddle_verb = copy.deepcopy(verb)
+		riddle_target = weighted_choice({"infinitive": 10, "past_simple": 40, "past_participle": 25})
+		expected_answer = riddle_verb.get(riddle_target).get("verb")
+		lenght = max([len(riddle_verb.get(riddle_target).get("verb")), len(riddle_verb.get(riddle_target).get("ipa"))])
+		riddle_placeholder = " "*int((lenght - 1)/2) + '?' + " "*int((lenght - 1)/2)
+		riddle_verb["past_simple"] = {"verb": riddle_placeholder, "ipa": riddle_placeholder}
+		score = victorina.draw(verb, riddle_verb, expected_answer, score)
 
-data[0][0]="FFFFF"
-table.replace_draw(1)
-GetLine.await_for_enter({" ": {"break": 1}})	
+sys.exit()
