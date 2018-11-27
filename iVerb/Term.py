ecaf180d52317a3a1ff53d3714ee1b86e499e319
turneps403 <turneps403@mail.ru>
2018-11-27 19:22:09 +0300

    flake8 started

--- iVerb/Term.py
+++ iVerb/Term.py
@@ -1,123 +1,122 @@
-import os,sys,tty,termios
 import copy
-import select as watcher
 import curses.ascii as keycode
+import os, sys, tty, termios
+import select as watcher
+
 from colorclass import Color
-from functools import reduce
+
 
 class GetKey:
-	@classmethod
-	def await_tap(cls):
-		fd = sys.stdin.fileno()
-		cur_settings = termios.tcgetattr(fd)
-		try:
-			tty.setraw(sys.stdin.fileno())
-			ch = ''
-			block = None
-			while True:
-				fd_sets = watcher.select([sys.stdin.fileno()], [], [], block)
-				if len(fd_sets[0]) > 0:
-					ch_chunk = os.read(sys.stdin.fileno(), 1)
-					ch += ch_chunk.decode("utf-8")
-					block = 0
-				else:
-					break
-		finally:
-			termios.tcsetattr(fd, termios.TCSADRAIN, cur_settings)
-		return ch
+    @classmethod
+    def await_tap(cls):
+        fd = sys.stdin.fileno()
+        cur_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = ""
+            block = None
+            while True:
+                fd_sets = watcher.select([sys.stdin.fileno()], [], [], block)
+                if len(fd_sets[0]) > 0:
+                    ch_chunk = os.read(sys.stdin.fileno(), 1)
+                    ch += ch_chunk.decode("utf-8")
+                    block = 0
+                else:
+                    break
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, cur_settings)
+        return ch
+
 
 class GetLine:
-	@classmethod
-	def await_for_enter(cls, tap_action=None):
-		right_position = 0
-		term_line = ''
-		while True:
-			tap_key = GetKey.await_tap()
-			if not tap_key.isprintable():
-				if tap_key == '\x1b[D':
-					# left arrow
-					if right_position < len(term_line):
-						right_position += 1
-						print("\b", end='', flush=True)
-						continue
-				elif tap_key == '\x1b[C':
-					# right arrow
-					if right_position > 0:
-						print(term_line[(len(term_line) - right_position):(len(term_line) - right_position + 1)], end='', flush=True)
-						right_position -= 1
-						continue
-				
-				tap_code = ord(tap_key[:1])
-				if tap_code in [keycode.BS, keycode.DEL]:
-					print("\b \b", end='', flush=True)
-				elif tap_code == keycode.CR:
-					if right_position > 0:
-						print_key = term_line[(len(term_line) - right_position):len(term_line)]
-						print(print_key, end='', flush=True)
-					term_line += tap_key
-					break
-				elif tap_code in [keycode.ETX, keycode.EOT, keycode.SUB]: # Ctrl+ C,D,Z
-					sys.exit()
-				elif tap_code == keycode.ESC and len(tap_key) == 1: # ESC
-					sys.exit()
-			else:
-				if right_position == 0:
-					print(tap_key, end='', flush=True)
-					term_line += tap_key 
-				else:
-					print_key = tap_key + term_line[(len(term_line) - right_position):len(term_line)]
-					print(print_key + "\b"*right_position, end='', flush=True)
-					term_line = term_line[0:(len(term_line) - right_position)] + tap_key + term_line[(len(term_line) - right_position):len(term_line)]
+    @classmethod
+    def await_for_enter(cls, tap_action=None):
+        right_position = 0
+        term_line = ""
+        while True:
+            tap_key = GetKey.await_tap()
+            if not tap_key.isprintable():
+                if tap_key == "\x1b[D":
+                    # left arrow
+                    if right_position < len(term_line):
+                        right_position += 1
+                        print("\b", end="", flush=True)
+                        continue
+                elif tap_key == "\x1b[C":
+                    # right arrow
+                    if right_position > 0:
+                        print(term_line[(len(term_line) - right_position):(len(term_line) - right_position + 1)], end="", flush=True)
+                        right_position -= 1
+                        continue
+                
+                tap_code = ord(tap_key[:1])
+                if tap_code in [keycode.BS, keycode.DEL]:
+                    print("\b \b", end="", flush=True)
+                elif tap_code == keycode.CR:
+                    if right_position > 0:
+                        print_key = term_line[(len(term_line) - right_position):len(term_line)]
+                        print(print_key, end="", flush=True)
+                    term_line += tap_key
+                    break
+                elif tap_code in [keycode.ETX, keycode.EOT, keycode.SUB]:  # Ctrl+ C,D,Z
+                    sys.exit()
+                elif tap_code == keycode.ESC and len(tap_key) == 1:  # ESC
+                    sys.exit()
+            else:
+                if right_position == 0:
+                    print(tap_key, end="", flush=True)
+                    term_line += tap_key 
+                else:
+                    print_key = tap_key + term_line[(len(term_line) - right_position):len(term_line)]
+                    print(print_key + "\b" * right_position, end="", flush=True)
+                    term_line = term_line[0:(len(term_line) - right_position)] + tap_key + term_line[(len(term_line) - right_position):len(term_line)]
 
-			if tap_action and tap_action.get(tap_key, None):
-				# just one action and go back
-				action = tap_action.get(tap_key)
-				if action.get("action", None):
-					action.get("action")(term_line)
-				if action.get("break", 0) > 0:
-					break
-			
-		return term_line
+            if tap_action and tap_action.get(tap_key, None):
+                # just one action and go back
+                action = tap_action.get(tap_key)
+                if action.get("action", None):
+                    action.get("action")(term_line)
+                if action.get("break", 0) > 0:
+                    break
+            
+        return term_line
+
 
 class HorizontalOptions:
-	def __init__(self, options, sep="    "):
-		self._options = options
-		self._sep = sep
+    def __init__(self, options, sep="    "):
+        self._options = options
+        self._sep = sep
 
-	def choice(self):
-		ind = None 
-		while True:
-			if ind is None:
-				ind = 0
-			else:
-				sys.stdout.write("\033[F\n\033[K")
-			options = copy.deepcopy(self._options)
-			real_ind = ind % len(options)
-			# clean
-			#print("len=" + str(( reduce(lambda a, x: a + len(x), options, 0) + len(self._sep)*(len(options)-1) )))
-			#sys.exit()
-			#sys.stdout.write("\033[K" + " "*( reduce(lambda a, x: a + len(x), options, 0) + len(self._sep)*(len(options)-1) ))
-			options[ real_ind ] = Color('{autobgwhite}'+options[ real_ind ]+'{/bgwhite}')
-			print(self._sep.join(options), end='', flush=True)
-			
-			tap_key = GetKey.await_tap()
-			if tap_key == '\x1b[D':
-				# left arrow
-				ind = len(options) - 1 if ind - 1 < 0 else ind - 1
-				continue
-			elif tap_key == '\x1b[C':
-				# right arrow
-				ind = ind + 1 if ind + 1 < len(options) else 0
-				continue
-			elif ord(tap_key[:1]) in [keycode.CR, ord(" ")]:
-				sys.stdout.write("\n")
-				return (ind % len(options))
-			else:
-				continue
+    def choice(self):
+        ind = None
+        while True:
+            if ind is None:
+                ind = 0
+            else:
+                sys.stdout.write("\033[F\n\033[K")
+            options = copy.deepcopy(self._options)
+            real_ind = ind % len(options)
+            options[real_ind] = Color("{autobgwhite}" + options[real_ind] + "{/bgwhite}")
+            print(self._sep.join(options), end="", flush=True)
+
+            tap_key = GetKey.await_tap()
+            if tap_key == "\x1b[D":
+                # left arrow
+                ind = len(options) - 1 if ind - 1 < 0 else ind - 1
+                continue
+            elif tap_key == "\x1b[C":
+                # right arrow
+                ind = ind + 1 if ind + 1 < len(options) else 0
+                continue
+            elif ord(tap_key[:1]) in [keycode.CR, ord(" ")]:
+                sys.stdout.write("\n")
+                return (ind % len(options))
+            else:
+                continue
 
 
-if __name__=='__main__':
-	choices = ["foo", "bar", "baz"]
-	opt = HorizontalOptions(choices)
-	print("Your choice is: " + choices[opt.choice()] )
-	#GetLine.await_for_enter()
+if __name__ == "__main__":
+    choices = ["foo", "bar", "baz"]
+    opt = HorizontalOptions(choices)
+    print("Your choice is: " + choices[opt.choice()])
+    # GetLine.await_for_enter()
--- iverb.py
+++ iverb.py
@@ -1,35 +1,39 @@
-import sys
+"""Console game and trainer."""
 import copy
 from random import uniform
+import sys
 
-from iVerb.Tables import IrregularVerbs, VictorinaTable, SimpleTable
+from iVerb.Tables import IrregularVerbs, SimpleTable, VictorinaTable
 from iVerb.Term import GetLine, HorizontalOptions
 
+
 def weighted_choice(choices):
-	total = sum(w for c, w in choices.items())
-	r = uniform(0, total)
-	upto = 0
-	for c, w in choices.items():
-		if upto + w >= r:
-			return c
-		upto += w
+    """Sample of weight balanced."""
+    total = sum(w for c, w in choices.items())
+    r = uniform(0, total)
+    upto = 0
+    for c, w in choices.items():
+        if upto + w >= r:
+            return c
+        upto += w
+
 
 choice = HorizontalOptions(["study", "test"]).choice()
 if choice == 0:
-	stable = SimpleTable()
-	for verb in iter(IrregularVerbs()):
-		stable.draw(verb)
-		GetLine.await_for_enter({" ": {"break": 1}, "\x1b[C": {"break": 1}})
+    stable = SimpleTable()
+    for verb in iter(IrregularVerbs()):
+        stable.draw(verb)
+        GetLine.await_for_enter({" ": {"break": 1}, "\x1b[C": {"break": 1}})
 else:
-	victorina = VictorinaTable()
-	score = 0
-	for verb in iter(IrregularVerbs()):
-		riddle_verb = copy.deepcopy(verb)
-		riddle_target = weighted_choice({"infinitive": 10, "past_simple": 40, "past_participle": 25})
-		expected_answer = riddle_verb.get(riddle_target).get("verb")
-		lenght = max([len(riddle_verb.get(riddle_target).get("verb")), len(riddle_verb.get(riddle_target).get("ipa"))])
-		riddle_placeholder = " "*(lenght//2) + '?' + " "*(lenght - 1 - (lenght//2))
-		riddle_verb["past_simple"] = {"verb": riddle_placeholder, "ipa": riddle_placeholder}
-		score = victorina.draw(verb, riddle_verb, expected_answer, score)
+    victorina = VictorinaTable()
+    score = 0
+    for verb in iter(IrregularVerbs()):
+        riddle_verb = copy.deepcopy(verb)
+        riddle_target = weighted_choice({"infinitive": 10, "past_simple": 40, "past_participle": 25})
+        expected_answer = riddle_verb.get(riddle_target).get("verb")
+        lenght = max([len(riddle_verb.get(riddle_target).get("verb")), len(riddle_verb.get(riddle_target).get("ipa"))])
+        riddle_placeholder = " " * (lenght // 2) + "?" + " " * (lenght - 1 - (lenght // 2))
+        riddle_verb["past_simple"] = {"verb": riddle_placeholder, "ipa": riddle_placeholder}
+        score = victorina.draw(verb, riddle_verb, expected_answer, score)
 
 sys.exit()
